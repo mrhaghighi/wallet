e99f9e63b7c915d7b259455e5a45fa1b32acbb6b
mrh_haghighi <haghighi.dev@gmail.com>
2021-09-21 00:34:14 +0430

    [test] Create some tests for increasing balance

---
+++ database/factories/BalanceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Balance;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BalanceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Balance::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->randomNumber(5),
+            'balance' => $this->faker->randomNumber(2)
+        ];
+    }
+}
---
+++ tests/IncreaseBalanceTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use App\Models\Balance;
+use App\Services\Balance\GetBallance;
+use App\Services\Balance\IncreaseBalance;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class IncreaseBalanceTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * test for increase balance with positive amount for valid user
+     *
+     * @return void
+     */
+    public function testIncreaseBalanceWithPositiveAmountForValidUser()
+    {
+        // Create balance
+        $balance = Balance::factory()->create();
+
+        // Increasing balance amount
+        $increasingAmount = rand(100, 999);
+
+        // Increase value
+        app()->make(IncreaseBalance::class)->increase($balance->user_id, $increasingAmount);
+
+        // Get new value
+        $newBalance = app()->make(GetBallance::class)->get($balance->user_id);
+
+        // Assert balances
+        $this->assertEquals(($balance->balance + $increasingAmount), $newBalance);
+    }
+
+    /**
+     * test for increase balance with negative amount for valid user
+     *
+     * @return void
+     */
+    public function testIncreaseBalanceWithNegativeAmountForValidUser()
+    {
+        // Create balance
+        $balance = Balance::factory()->create();
+
+        // Increasing balance amount
+        $increasingAmount = rand(-1, -999);
+
+        // Assert increasing value
+        $this->expectException(HttpException::class);
+        app()->make(IncreaseBalance::class)->increase($balance->user_id, $increasingAmount);
+    }
+
+    /**
+     * test for increase balance with positive amount for not valid user
+     *
+     * @return void
+     */
+    public function testIncreaseBalanceWithPositiveAmountForNotalidUser()
+    {
+        // Create balance
+        $maxUserIdInBalances = Balance::max('user_id');
+        $notValidUserId = $maxUserIdInBalances + rand(100, 1000);
+
+        // Increasing balance amount
+        $increasingAmount = rand(100, 999);
+
+        // Assert increasing value
+        $this->expectException(ModelNotFoundException::class);
+        app()->make(IncreaseBalance::class)->increase($notValidUserId, $increasingAmount);
+    }
+}
