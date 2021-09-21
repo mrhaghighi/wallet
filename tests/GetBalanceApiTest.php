fa4f20071132603b9dd737fe01603d94380f098e
mrh_haghighi <haghighi.dev@gmail.com>
2021-09-21 11:39:08 +0430

    [test] Crete some tests for getting balance api

---
+++ tests/GetBalanceApiTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Balance;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class GetBalanceApiTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * test getting balance for a valid user
+     *
+     * @return void
+     */
+    public function testGettingBalanceForValidUserViaApi()
+    {
+        // Create a balance
+        $balance = Balance::factory()->create();
+
+        $this->json('GET', "/balance/{$balance->user_id}")
+            ->seeJson([
+                'status' => 'success'
+            ])
+            ->assertResponseOk();
+    }
+
+    /**
+     * test getting balance for a not valid user
+     *
+     * @return void
+     */
+    public function testGettingBalanceForNotValidUserViaApi()
+    {
+        // Create a balance
+        $maxUserId = Balance::max('user_id');
+        $notValidUserId = $maxUserId + rand(100, 1000);
+
+        $this->json('GET', "/balance/{$notValidUserId}")
+            ->assertResponseStatus(404);
+    }
+}
